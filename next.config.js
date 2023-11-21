bdedfa8b598f51132deedc8f6bc3208f5984394c
Tatiana <tanya18082000@mail.ru>
2023-11-22 00:52:06 +0300

    feature: dockerfile

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    API_URL: "http://localhost:4200",
-  },
+    env: {
+        API_URL: "http://localhost:4200",
+    },
+    output: "standalone",
 };
 
 module.exports = nextConfig;
